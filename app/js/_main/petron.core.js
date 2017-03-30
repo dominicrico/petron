533890b1fe4442922d669b902ebc9ce96ca5dd6e
Dominic Rico-Gomez <hello@coderocket.me>
2017-03-31 00:21:47 +0200

    added sweetalert

--- app/js/_main/petron.core.js
+++ app/js/_main/petron.core.js
@@ -1,38 +1,39 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular.module('petron.core.dependencies', [
-		'angular-electron',
-		'ui.router',
-		'pascalprecht.translate',
-		'ngDialog',
-		'angularRangeSlider',
-		'hmTouchEvents',
-		'nemLogging',
-		'ui-leaflet',
-		'tmh.dynamicLocale'
-	]);
+  angular.module('petron.core.dependencies', [
+    'angular-electron',
+    'ui.router',
+    'pascalprecht.translate',
+    'ngDialog',
+    'angularRangeSlider',
+    'hmTouchEvents',
+    'nemLogging',
+    'ui-leaflet',
+    'tmh.dynamicLocale',
+    'oitozero.ngSweetAlert'
+  ]);
 
-	angular.module('petron.core.modules', []);
-	angular.module('petron.core.modules')
-		.config(['remoteProvider', function(remoteProvider) {
-			remoteProvider.register('fs');
-			remoteProvider.register('path');
-			remoteProvider.register('musicmetadata');
-			remoteProvider.register('ffmpeg');
-			remoteProvider.register('ffprobe');
-			remoteProvider.register('ffprobe-static');
-			remoteProvider.register('electron-json-storage');
-			//remoteProvider.register('node-rpi-si4703');
-			remoteProvider.register('node-obd');
-			remoteProvider.register('obd-parser-development-connection');
-		}]);
+  angular.module('petron.core.modules', []);
+  angular.module('petron.core.modules')
+    .config(['remoteProvider', function(remoteProvider) {
+      remoteProvider.register('fs');
+      remoteProvider.register('path');
+      remoteProvider.register('musicmetadata');
+      remoteProvider.register('ffmpeg');
+      remoteProvider.register('ffprobe');
+      remoteProvider.register('ffprobe-static');
+      remoteProvider.register('electron-json-storage');
+      //remoteProvider.register('node-rpi-si4703');
+      remoteProvider.register('node-obd');
+      remoteProvider.register('obd-parser-development-connection');
+    }]);
 
-	angular.module('petron.core', [
-		'petron.core.dependencies',
-		'petron.core.config',
-		'petron.core.modules',
-		'petron.core.templates',
-		'petron.core.routes'
-	]);
+  angular.module('petron.core', [
+    'petron.core.dependencies',
+    'petron.core.config',
+    'petron.core.modules',
+    'petron.core.templates',
+    'petron.core.routes'
+  ]);
 })();
